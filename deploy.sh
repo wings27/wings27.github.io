43988f5a08c981ee1a975b44b20ff187183c0efb
wenqiushi <wenqiushi@meituan.com>
2020-04-03 20:52:29 +0800

    travis ci

--- deploy.sh
+++ deploy.sh
@@ -23,7 +23,7 @@ fi
 
 # Save some useful information
 REPO=`git config remote.origin.url`
-SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SSH_REPO=${GH_TOKEN}${REPO/https:\/\/github.com\//@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
 git config remote.origin.fetch refs/heads/*:refs/remotes/origin/*
@@ -59,14 +59,15 @@ git log -n 10 --graph --pretty=oneline --abbrev-commit --decorate --date=relativ
 echo;
 
 # Get the deploy key through Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
+# ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+# ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+# ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+# ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+# openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+# chmod 600 deploy_key
+# eval `ssh-agent -s`
+# ssh-add deploy_key
 
+echo $SSH_REPO
 # All set up. Push to remote.
 git push --verbose $SSH_REPO $TARGET_BRANCH
